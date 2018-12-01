93ec4ff524af9f5d4f8389e50ca7c58912cbc902
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-12-01 11:17:43 +0100

    1b works, but can we do this nicer?

---
+++ 1/src/1b.ts
@@ -0,0 +1,27 @@
+
+import * as fs from "fs";
+
+fs.readFile("input.txt", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const changes = data
+    .toString()
+    .split("\n")
+    .map(s => parseInt(s));
+  const seen: boolean[] = [];
+  let frequency = 0;
+  let i = 0;
+  for (;;) {
+    if (seen[frequency]) {
+      console.info('found', frequency);
+      return;
+    }
+    seen[frequency] = true;
+    frequency += changes[i];
+    i++;
+    if (i >= changes.length) {
+      i = 0;
+    }
+  }
+});
