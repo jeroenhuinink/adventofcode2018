d7e69852f32e4b65944a97862286e1cd9eeb746b
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-12-02 09:57:21 +0100

    Why do we need the generator in the first place. Wouild be soooo much nicer if we could write [1..].reduce(fn) and typescript/javascript would have lazy evaluation and list comprehensions

--- 1/src/1b.ts
+++ 1/src/1b.ts
@@ -10,22 +10,15 @@ fs.readFile("input.txt", (err, data) => {
     .split("\n")
     .map(s => parseInt(s));
 
-  let i = -1;
-  function* getChanges() {
-    i++;
-    yield changes[i % changes.length];
-  }
-
   const seen: boolean[] = [];
   let frequency = 0;
-  for (;;) {
-    for(let value of getChanges()) {
-      if (seen[frequency]) {
-        console.info(frequency);
-        return;
-      }
-      seen[frequency] = true;
-      frequency += value;
+  for (let i = 0; ; i++) {
+    const value = changes[i % changes.length];
+    if (seen[frequency]) {
+      console.info(frequency);
+      return;
     }
+    seen[frequency] = true;
+    frequency += value;
   }
 });
