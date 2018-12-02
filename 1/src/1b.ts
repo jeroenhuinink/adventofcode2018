475784d292e885fa65be486bc910226823ef78d9
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-12-02 09:03:24 +0100

    using generator function but still not where I want it

--- 1/src/1b.ts
+++ 1/src/1b.ts
@@ -1,27 +1,31 @@
-
 import * as fs from "fs";
 
 fs.readFile("input.txt", (err, data) => {
   if (err) {
     throw err;
   }
+
   const changes = data
     .toString()
     .split("\n")
     .map(s => parseInt(s));
+
+  let i = -1;
+  function* getChanges() {
+    i++;
+    yield changes[i % changes.length];
+  }
+
   const seen: boolean[] = [];
   let frequency = 0;
-  let i = 0;
   for (;;) {
-    if (seen[frequency]) {
-      console.info(frequency);
-      return;
-    }
-    seen[frequency] = true;
-    frequency += changes[i];
-    i++;
-    if (i >= changes.length) {
-      i = 0;
+    for(let value of getChanges()) {
+      if (seen[frequency]) {
+        console.info(frequency);
+        return;
+      }
+      seen[frequency] = true;
+      frequency += value;
     }
   }
 });
