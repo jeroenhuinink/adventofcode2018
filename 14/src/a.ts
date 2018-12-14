a5488896eae8ef5193d6b0e59dee94bdb90ea47a
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-12-14 12:17:06 +0100

    Solved 14

--- 14/src/a.ts
+++ 14/src/a.ts
@@ -1,14 +1,58 @@
 import * as fs from "fs";
 
-fs.readFile("input.txt", (err, data) => {
-  if (err) {
-    throw err;
-  }
+let scoreboard: number[] = [];
+let elves = [0, 1];
 
-  const lines = data
+function createRecipe() {
+  const score = elves.map(e => scoreboard[e]).reduce((a, v) => a + v, 0);
+  const newReceipies = score
     .toString()
-    .split("\n")
-    
+    .split("")
+    .map(s => parseInt(s));
+  newReceipies.forEach(r => scoreboard.push(r));
+  elves = elves.map(e => (e + (scoreboard[e] + 1)) % scoreboard.length);
+}
 
-  console.info(lines);
-});
+function logScoreboard() {
+  console.log(elves);
+  console.log(
+    scoreboard
+      .map((s, i) =>
+        elves[0] == i
+          ? "(" + s.toString() + ")"
+          : elves[1] == i
+          ? "[" + s.toString() + "]"
+          : " " + s.toString() + " "
+      )
+      .join("")
+  );
+}
+
+function solve(initial: string, count: number): string {
+  scoreboard = initial
+    .toString()
+    .split("")
+    .map(s => parseInt(s));
+  elves = [0, 1];
+  while (scoreboard.length < count + 10) {
+    createRecipe();
+    // logScoreboard();
+  }
+  return scoreboard
+    .slice(count, count + 10)
+    .join("");
+}
+
+function test(initial: string, count: number, expected:string) {
+  const actual = solve(initial, count);
+  if (actual != expected) {
+    console.error(`Expected ${expected} got ${actual}`);
+  }
+}
+
+test("37", 5, "0124515891");
+test("37", 9, "5158916779");
+test("37", 18, "9251071085");
+test("37", 2018, "5941429882");
+
+console.info(solve("37", 165061));
--- 14/src/b.ts
+++ 14/src/b.ts
@@ -1,14 +1,73 @@
 import * as fs from "fs";
 
-fs.readFile("input.txt", (err, data) => {
-  if (err) {
-    throw err;
+let scoreboard: number[] = [];
+let elves = [0, 1];
+
+function createRecipe(length: number, last: string): string {
+  const score = scoreboard[elves[0]] + scoreboard[elves[1]];
+  if (score < 10) {
+    scoreboard.push(score);
+    last = (last + score.toString()).substr(-(length + 1));
+  } else {
+    scoreboard.push(1);
+    scoreboard.push(score - 10);
+    last = (last + "1" + (score - 10).toString()).substr(-(length + 1));
   }
+  const e0 = elves[0];
+  elves[0] = (e0 + (scoreboard[e0] + 1)) % scoreboard.length;
+  const e1 = elves[1];
+  elves[1] = (e1 + (scoreboard[e1] + 1)) % scoreboard.length;
+  return last;
+}
 
-  const lines = data
+function logScoreboard() {
+  console.log(elves);
+  console.log(
+    scoreboard
+      .map((s, i) =>
+        elves[0] == i
+          ? "(" + s.toString() + ")"
+          : elves[1] == i
+          ? "[" + s.toString() + "]"
+          : " " + s.toString() + " "
+      )
+      .join("")
+  );
+}
+
+function solve(initial: string, match: string): number {
+  scoreboard = initial
     .toString()
-    .split("\n")
-    
+    .split("")
+    .map(s => parseInt(s));
+  elves = [0, 1];
+  let i = 0;
+  let last = initial;
+  while (
+    last.substr(0, match.length) !== match &&
+    last.substr(1, match.length) !== match
+  ) {
+    i++;
+    last = createRecipe(match.length, last);
+  }
+  if (last.substr(0, match.length) === match) {
+    return scoreboard.length - match.length - 1;
+  } else {
+    return scoreboard.length - match.length;
+  }
+}
 
-  console.info(lines);
-});
+function test(initial: string, match: string, expected: number) {
+  const actual = solve(initial, match);
+  if (actual != expected) {
+    console.error(`Expected ${expected} got ${actual}`);
+    throw "fail";
+  }
+}
+
+test("37", "012451", 5);
+test("37", "51589", 9);
+test("37", "92510", 18);
+test("37", "59414", 2018);
+
+console.info(solve("37", "165061"));
