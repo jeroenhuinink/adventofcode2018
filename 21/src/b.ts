4f08023b9d452670d967fd949896d9e2f27487d9
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-12-14 13:13:10 +0100

    Updated template with test setup

--- 21/src/b.ts
+++ 21/src/b.ts
@@ -1,14 +1,26 @@
 import * as fs from "fs";
 
+function solve(input: string): any {
+  return 0;
+}
+
+let testId = 0;
+function test(input: string, expected: any) {
+  testId++;
+  const actual = solve(input);
+  if (actual != expected) {
+    console.error(`${testId} failed. Expected ${expected} got ${actual}`);
+    throw "fail";
+  }
+}
+
+test("", 0);
+
 fs.readFile("input.txt", (err, data) => {
   if (err) {
     throw err;
   }
 
-  const lines = data
-    .toString()
-    .split("\n")
-    
-
-  console.info(lines);
+  const input = data.toString();
+  console.info(solve(input));
 });
