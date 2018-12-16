0794ff6498e07eefc853568dce3a0413df03c8a1
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-12-16 12:54:17 +0100

    Solved 16

--- 16/src/a.ts
+++ 16/src/a.ts
@@ -1,7 +1,139 @@
 import * as fs from "fs";
 
+function parseInstruction(id: number) {
+  return id;
+}
+
+function process(
+  instruction: number,
+  lhs: number,
+  rhs: number,
+  output: number,
+  registers: number[]
+): number[] {
+  const result = [...registers];
+  function getRegister(id: number) {
+    return result[id];
+  }
+
+  function setRegister(id: number, value: number) {
+    result[id] = value;
+  }
+  
+  switch (instruction) {
+    case 0:
+      //addr
+      setRegister(output, getRegister(lhs) + getRegister(rhs));
+      break;
+    case 1:
+      //addi
+      setRegister(output, getRegister(lhs) + rhs);
+      break;
+    case 2:
+      //mulr
+      setRegister(output, getRegister(lhs) * getRegister(rhs));
+      break;
+    case 3:
+      //muli
+      setRegister(output, getRegister(lhs) * rhs);
+      break;
+    case 4:
+      //banr
+      setRegister(output, getRegister(lhs) & getRegister(rhs));
+      break;
+    case 5:
+      //bani
+      setRegister(output, getRegister(lhs) & rhs);
+      break;
+    case 6:
+      //borr
+      setRegister(output, getRegister(lhs) | getRegister(rhs));
+      break;
+    case 7:
+      //bori
+      setRegister(output, getRegister(lhs) | rhs);
+      break;
+    case 8:
+      //setr
+      setRegister(output, getRegister(lhs));
+      break;
+    case 9:
+      //seti
+      setRegister(output, lhs);
+      break;
+    case 10:
+      //gtir
+      setRegister(output, lhs > getRegister(rhs) ? 1 : 0);
+      break;
+    case 11:
+      //gtri
+      setRegister(output, getRegister(lhs) > rhs ? 1 : 0);
+      break;
+    case 12:
+      //gtrr
+      setRegister(output, getRegister(lhs) > getRegister(rhs) ? 1 : 0);
+      break;
+    case 13:
+      //eqir
+      setRegister(output, lhs === getRegister(rhs) ? 1 : 0);
+      break;
+    case 14:
+      //eqri
+      setRegister(output, getRegister(lhs) === rhs ? 1 : 0);
+      break;
+    case 15:
+      //eqrr
+      setRegister(output, getRegister(lhs) === getRegister(rhs) ? 1 : 0);
+      break;
+    default:
+      throw `Unknown instruction: ${instruction}`;
+  }
+  return result;
+}
+
+function matches(
+  before: number[],
+  instruction: number[],
+  expected: number[]
+): number {
+  let matches = 0;
+  for (let i = 0; i < 16; i++) {
+    const after = process(
+      i,
+      instruction[1],
+      instruction[2],
+      instruction[3],
+      before
+    );
+    if (after.every((v, i) => expected[i] == v)) {
+      matches++;
+    }
+    
+  }
+  return matches;
+}
+
 function solve(input: string): any {
-  return 0;
+  let solution = 0;
+  const lines = input.split("\n");
+  let i = 0;
+  while (i < lines.length) {
+    const before = lines[i]
+      .substr(9, 10)
+      .split(",")
+      .map(s => parseInt(s));
+    const instruction = lines[i + 1].split(" ").map(s => parseInt(s));
+    const after = lines[i + 2]
+      .substr(9, 10)
+      .split(",")
+      .map(s => parseInt(s));
+    i += 4;
+    const count = matches(before, instruction, after);
+    if (count > 2) {
+      solution++;
+    }
+  }
+  return solution;
 }
 
 let testId = 0;
@@ -14,13 +146,19 @@ function test(input: string, expected: any) {
   }
 }
 
-test("", 0);
+test(
+  `Before: [3, 2, 1, 1]
+9 2 1 2
+After:  [3, 2, 2, 1]
+`,
+  1
+);
 
-fs.readFile("input.txt", (err, data) => {
+fs.readFile("input.a.txt", (err, data) => {
   if (err) {
     throw err;
   }
-
   const input = data.toString();
+
   console.info(solve(input));
 });
--- 16/src/b.ts
+++ 16/src/b.ts
@@ -1,26 +1,203 @@
 import * as fs from "fs";
 
-function solve(input: string): any {
-  return 0;
+let instructionList = [
+  "addr",
+  "addi",
+  "mulr",
+  "muli",
+  "banr",
+  "bani",
+  "borr",
+  "bori",
+  "setr",
+  "seti",
+  "gtir",
+  "gtri",
+  "gtrr",
+  "eqri",
+  "eqir",
+  "eqrr"
+];
+
+function process(
+  instruction: string,
+  lhs: number,
+  rhs: number,
+  output: number,
+  registers: number[]
+): number[] {
+  const result = [...registers];
+  function getRegister(id: number) {
+    return result[id];
+  }
+
+  function setRegister(id: number, value: number) {
+    result[id] = value;
+  }
+
+  switch (instruction) {
+    case "addr":
+      setRegister(output, getRegister(lhs) + getRegister(rhs));
+      break;
+    case "addi":
+      setRegister(output, getRegister(lhs) + rhs);
+      break;
+    case "mulr":
+      setRegister(output, getRegister(lhs) * getRegister(rhs));
+      break;
+    case "muli":
+      setRegister(output, getRegister(lhs) * rhs);
+      break;
+    case "banr":
+      setRegister(output, getRegister(lhs) & getRegister(rhs));
+      break;
+    case "bani":
+      setRegister(output, getRegister(lhs) & rhs);
+      break;
+    case "borr":
+      setRegister(output, getRegister(lhs) | getRegister(rhs));
+      break;
+    case "bori":
+      setRegister(output, getRegister(lhs) | rhs);
+      break;
+    case "setr":
+      setRegister(output, getRegister(lhs));
+      break;
+    case "seti":
+      setRegister(output, lhs);
+      break;
+    case "gtir":
+      setRegister(output, lhs > getRegister(rhs) ? 1 : 0);
+      break;
+    case "gtri":
+      setRegister(output, getRegister(lhs) > rhs ? 1 : 0);
+      break;
+    case "gtrr":
+      setRegister(output, getRegister(lhs) > getRegister(rhs) ? 1 : 0);
+      break;
+    case "eqir":
+      setRegister(output, lhs === getRegister(rhs) ? 1 : 0);
+      break;
+    case "eqri":
+      setRegister(output, getRegister(lhs) === rhs ? 1 : 0);
+      break;
+    case "eqrr":
+      setRegister(output, getRegister(lhs) === getRegister(rhs) ? 1 : 0);
+      break;
+    default:
+      throw `Unknown instruction: ${instruction}`;
+  }
+  return result;
+}
+
+const instructionMap: string[] = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null
+];
+
+function matches(
+  before: number[],
+  instruction: number[],
+  expected: number[]
+): number {
+  let found: { source: number; target: string }[] = [];
+
+  for (let i = 0; i < instructionList.length; i++) {
+    const after = process(
+      instructionList[i],
+      instruction[1],
+      instruction[2],
+      instruction[3],
+      before
+    );
+    if (after.every((v, i) => expected[i] == v)) {
+      found.push({ source: instruction[0], target: instructionList[i] });
+    }
+  }
+  if (found.length == 1) {
+    instructionMap[found[0].source] = found[0].target;
+    instructionList = instructionList.filter(v => v != found[0].target);
+  }
+
+  return instructionList.length;
+}
+
+function findInstructionMap(input: string) {
+  const lines = input.split("\n");
+
+  let unresolved = 16;
+  while (unresolved > 0) {
+    let i = 0;
+    while (i < lines.length) {
+      const before = lines[i]
+        .substr(9, 10)
+        .split(",")
+        .map(s => parseInt(s));
+      const instruction = lines[i + 1].split(" ").map(s => parseInt(s));
+      const after = lines[i + 2]
+        .substr(9, 10)
+        .split(",")
+        .map(s => parseInt(s));
+      i += 4;
+      if (!instructionMap[instruction[0]]) {
+        unresolved = matches(before, instruction, after);
+      }
+    }
+  }
 }
 
 let testId = 0;
 function test(input: string, expected: any) {
   testId++;
-  const actual = solve(input);
+  const actual = findInstructionMap(input);
   if (actual != expected) {
     console.error(`${testId} failed. Expected ${expected} got ${actual}`);
     throw "fail";
   }
 }
 
-test("", 0);
+// test(
+//   `Before: [3, 2, 1, 1]
+// 9 2 1 2
+// After:  [3, 2, 2, 1]
+// `,
+//   1
+// );
 
-fs.readFile("input.txt", (err, data) => {
+fs.readFile("input.a.txt", (err, data) => {
   if (err) {
     throw err;
   }
-
   const input = data.toString();
-  console.info(solve(input));
+  findInstructionMap(input);
+
+  fs.readFile("input.b.txt", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const instructions = data
+      .toString()
+      .split("\n")
+      .map(s => s.split(" ").map(i => parseInt(i)));
+    
+    let registers = [0, 0, 0, 0];
+    instructions.forEach((instr,i) => {
+      registers = process(instructionMap[instr[0]], instr[1], instr[2], instr[3], registers);
+    });
+    console.info(registers[0]);
+  });
 });
